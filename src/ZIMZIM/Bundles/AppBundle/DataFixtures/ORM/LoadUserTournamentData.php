f2fa51f739e187e844be6ad2eb30caa7d8e22b5f
zimzim62000 <zimzim62000@gmail;com>
2014-06-28 00:10:05 +0200

    ready to rumble

--- src/ZIMZIM/Bundles/AppBundle/DataFixtures/ORM/LoadUserTournamentData.php
+++ src/ZIMZIM/Bundles/AppBundle/DataFixtures/ORM/LoadUserTournamentData.php
@@ -15,8 +15,8 @@ class LoadUserTournamentData extends AbstractFixture implements OrderedFixtureIn
     {
         $tournament = new UserTournament();
         $tournament->setName('Centaure systems : FIFA WORLD CUP 2014');
-        $tournament->setText('Régles: 1point pour victoire/null , 3 points pour score exacte,
-        Attention le score final du match est celui à la fin du match. ( si prolongation le score finale est celui après les prolongations ) ');
+        $tournament->setText('Régles: 1point pour victoire/null , 3 points pour score exact,
+        Attention en cas de finish au pénalty, le score compte jusqu\'au bout des 120 minutes. Alors tous le monde est préts à parier? A trés bientôt sur parier entre amis !');
         $tournament->setDateStart(new \DateTime('2014-07-28'));
         $tournament->setDateEnd(new \DateTime('2014-07-13'));
         $tournament->setEnabled(true);
